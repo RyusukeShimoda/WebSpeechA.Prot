271d121ac0d9603e1c06e6e32c9e04b1dbfbade7
R.Shimoda <m0a20146d1@edu.teu.ac.jp>
2023-05-09 02:34:26 +0900

    Prime

---
+++ Prototype/script.js
@@ -0,0 +1,5 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
