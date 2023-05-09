c8bb57b3466e7b4410b597bc92e8f1fd7b12894b
R.Shimoda <m0a20146d1@edu.teu.ac.jp>
2023-05-09 15:17:40 +0900

    Options

--- Prototype/script.js
+++ Prototype/script.js
@@ -3,3 +3,12 @@ var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+recognition.continuous = true;
+//言語設定
+//recognition.lang = 'en-US';
+//中途認識
+recognition.interimResults = true;
+//代替候補数（2以上で代替候補リスト表示）
+recognition.maxAlternatives = 1;
